760fcaed132893fc0066408897c318f6eb9e4ff1
lh-ycx <lh_ycx@126.com>
2018-03-25 15:25:17 +0800

    add register and resolve modules, to add node, please input '127.0.0.1:port' instead of '0.0.0.0:port'!

--- blockchain.py
+++ blockchain.py
@@ -18,6 +18,10 @@ class MyForm(Form):
     amount = DecimalField('amount',validators=[DataRequired()])
     submit = SubmitField('submit')
 
+class Register_Form(Form):
+    node = StringField('node', validators=[DataRequired()])
+    submit = SubmitField('submit')
+
 class Blockchain(object):
     def __init__(self):
         self.current_transactions = []
@@ -37,8 +41,14 @@ class Blockchain(object):
         :return: None
         """
 
-        parsed_url = urlparse(address)
-        self.nodes.add(parsed_url.netloc)
+        print('------------------check address-----------------')
+        print(address)
+
+        #parsed_url = urlparse(address)
+        #self.nodes.add(parsed_url.netloc)
+        self.nodes.add(address)
+        print('------------------check nodes-----------------')
+        print(self.nodes)
 
     
     def valid_chain(self, chain):
@@ -84,7 +94,7 @@ class Blockchain(object):
 
         # Grab and verify the chains from all the nodes in our network
         for node in neighbours:
-            response = requests.get(f'http://{node}/chain')
+            response = requests.get(f'http://{node}/pure_chain')
 
             if response.status_code == 200:
                 length = response.json()['length']
@@ -254,13 +264,22 @@ def full_chain():
     return render_template("chain.html",chain=response)
     
 
+@app.route('/pure_chain', methods=['GET'])
+def pure_chain():
+    response = {
+        'chain': blockchain.chain,
+        'length': len(blockchain.chain),
+    }
+    return jsonify(response), 200
+
 @app.route('/', methods=['GET'])
 def hello():
     return render_template("index.html")
 
 
-@app.route('/register', methods=['POST'])
+@app.route('/register', methods=['GET','POST'])
 def register_nodes():
+    '''
     values = request.get_json()
 
     nodes = values.get('nodes')
@@ -275,6 +294,15 @@ def register_nodes():
         'total_nodes': list(blockchain.nodes),
     }
     return jsonify(response), 201
+    '''
+    form = Register_Form(csrf_enabled=False)
+    if form.validate_on_submit():
+        node = form.data['node']
+        blockchain.register_node(node)
+        response = {'message': f'new node {node} has been recorded'}
+        return render_template("register.html",message=response)
+
+    return render_template('register.html',form=form)
 
 
 @app.route('/resolve', methods=['GET'])
@@ -292,8 +320,16 @@ def consensus():
             'chain': blockchain.chain
         }
 
-    return jsonify(response), 200
+    return render_template("resolve.html",message=response)
+    #return jsonify(response), 200
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    from argparse import ArgumentParser
+
+    parser = ArgumentParser()
+    parser.add_argument('-p', '--port', default=5000, type=int, help='port to listen on')
+    args = parser.parse_args()
+    port = args.port
+
+    app.run(host='0.0.0.0', port=port)
