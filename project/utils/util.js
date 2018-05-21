0e6fcb5f9fe20cfef5c8e9242f6a7bd265c33181
zhackzey <32985806+zhackzey@users.noreply.github.com>
2018-05-21 22:02:16 +0800

    1.0

--- project/utils/util.js
+++ project/utils/util.js
@@ -14,6 +14,74 @@ const formatNumber = n => {
   return n[1] ? n : '0' + n
 }
 
+var rootDocment = 'https://162.105.175.115';
+function req(url, data, cb) {
+  wx.request({
+    url: rootDocment + url,
+    data: data,
+    method: 'post',
+    header: { 'Content-Type': 'application/x-www-form-urlencoded' },
+    success: function (res) {
+      return typeof cb == "function" && cb(res.data)
+    },
+    fail: function () {
+      return typeof cb == "function" && cb(false)
+    }
+  })
+}
+
+function getReq(url, data, cb) {
+  wx.request({
+    url: rootDocment + url,
+    data: data,
+    method: 'get',
+    header: { 'Content-Type': 'application/x-www-form-urlencoded' },
+    success: function (res) {
+      return typeof cb == "function" && cb(res.data)
+    },
+    fail: function () {
+      return typeof cb == "function" && cb(false)
+    }
+  })
+}
+
+// 去前后空格  
+function trim(str) {
+  return str.replace(/(^\s*)|(\s*$)/g, "");
+}
+
+// 提示错误信息  
+function isError(msg, that) {
+  that.setData({
+    showTopTips: true,
+    errorMsg: msg
+  })
+}
+
+function islegal(str){
+  //var reg = /^1[1-8]0{3}[0-9]{5}$/;
+  return str.match(/^1[1-8]0{3}[0-9]{5}$/);
+}
+
+// 清空错误信息  
+function clearError(that) {
+  that.setData({
+    showTopTips: false,
+    errorMsg: ""
+  })
+}
+
+function isStudentNo(str) {
+  console.log(this)
+  return str;
+}
+
 module.exports = {
-  formatTime: formatTime
+  formatTime: formatTime,
+  req: req,
+  trim: trim,
+  islegal:islegal,
+  isError: isError,
+  clearError: clearError,
+  getReq: getReq
 }
