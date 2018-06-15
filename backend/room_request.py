8e7b8a3efad5bafb241bb5e1ad48a8b304122135
zhackzey <32985806+zhackzey@users.noreply.github.com>
2018-06-15 22:37:31 +0800

    格式化代码

--- backend/room_request.py
+++ backend/room_request.py
@@ -16,17 +16,12 @@ class Room_request(object):
 
     def getName(self):
         return self.name
-'''
-    def getSubarea(self):
-        return self.subarea
-    def getDescription(self):
-        return self.description
-'''
+
     def getCourseId(self):
         return self.courseId
 
     def setTime(self):
-        #将时间更改为当前时间，总是返回True
+        # 将时间更改为当前时间，总是返回True
         self.time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         return True
 
@@ -36,38 +31,15 @@ class Room_request(object):
         return True
         # print("error: ", name, " is not a string!")
         # return False
-'''
-    def setSubarea(self, subarea):
-        # if Usr.checkPre(subarea):
-        self.subarea = subarea
-        return True
-        # print("error: ", subarea, " is not an available subarea!")
-        # return False
 
-    def setDescription(self,description):
-        # if isinstance(description, str):
-        self.description = description
-        return True
-        # print("error: description must be str!")
-        # return False
-'''
     def setCourseId(self, courseId):
         self.courseId = courseId
 
-
     def checkReq(self):
-        #查看该request实例是否满足创建房间的需求
+        # 查看该request实例是否满足创建房间的需求
         if not isinstance(self.name, str) or self.name == "":
             print("error: check room name failed!")
             return False
-'''
-        if not Usr.checkPre(self.subarea):
-            print("error: check room subarea failed!")
-            return False
-        if not isinstance(self.description, str) or self.description == "":
-            print("error: check room description failed!")
-            return False
-'''
         if self.courseId < 0:
             print("error: check courseId failed!")
             return False
